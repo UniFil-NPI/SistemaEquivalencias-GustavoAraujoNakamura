830aa54ada49f92b1e043890369571270d4e03df
Nakamura <gustavo.nakamura@edu.unifil.br>
2024-05-22 15:17:06 -0300

    Deus é bom 3

--- resources/js/Pages/DisciplinaCreate.vue
+++ resources/js/Pages/DisciplinaCreate.vue
@@ -47,7 +47,7 @@ export default {
             this.carregarDisciplinas();
         },
         async atualizarDisciplina() {
-            await fetch(route('disciplina.edit',this.disciplinaAtual.id), {
+            await fetch(route('disciplina.edit', this.disciplinaAtual.id), {
                 method: 'PUT',
                 headers: {
                     'Content-Type': 'application/json',
@@ -70,13 +70,17 @@ export default {
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="codigo">
                         Código da disciplina
                     </label>
-                    <input v-model="disciplinaAtual.codigo" class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="codigo" name="codigo" type="text" placeholder="Código da disciplina" required>
+                    <input v-model="disciplinaAtual.codigo"
+                           class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                           id="codigo" name="codigo" type="text" placeholder="Código da disciplina" required>
                 </div>
                 <div class="w-full px-6">
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="titulo">
                         Título da disciplina
                     </label>
-                    <input v-model="disciplinaAtual.titulo" class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="titulo" name="titulo" type="text" placeholder="Título da disciplina" required>
+                    <input v-model="disciplinaAtual.titulo"
+                           class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                           id="titulo" name="titulo" type="text" placeholder="Título da disciplina" required>
                 </div>
             </div>
             <div class="flex flex-wrap -mx-6 mb-6">
@@ -84,31 +88,46 @@ export default {
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="tipo">
                         Tipo da disciplina
                     </label>
-                    <input v-model="disciplinaAtual.tipo" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="tipo" name="tipo" type="text" placeholder="Tipo da disciplina" required>
+                    <select v-model="disciplinaAtual.tipo"
+                            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="tipo" name="tipo" required>
+                        <option value="">Tipo da disciplina</option>
+                        <option value="Core" :selected="disciplinaAtual.tipo === 'Core'">Core</option>
+                        <option value="Flex" :selected="disciplinaAtual.tipo === 'Flex'">Flex</option>
+                    </select>
                 </div>
                 <div class="w-full md:w-1/2 px-6">
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="periodo">
                         Período da disciplina
                     </label>
-                    <input v-model="disciplinaAtual.periodo" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="periodo" name="periodo" type="text" placeholder="Período da disciplina" required>
+                    <input v-model.number="disciplinaAtual.periodo"
+                           class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                           id="periodo" name="periodo" type="number" placeholder="Período da disciplina" required>
                 </div>
             </div>
             <div class="flex flex-wrap -mx-6 mb-2">
                 <div class="w-full px-6 mb-6 md:mb-0">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="carga_horaria">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                           for="carga_horaria">
                         Carga horária
                     </label>
-                    <input v-model="disciplinaAtual.carga_horaria" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="carga_horaria" name="carga_horaria" type="text" placeholder="Carga horária" required>
+                    <input v-model.number="disciplinaAtual.carga_horaria"
+                           class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                           id="carga_horaria" name="carga_horaria" type="number" placeholder="Carga horária" required>
                 </div>
                 <div class="w-full px-6 mb-6 md:mb-0">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" id="ativo" name="ativo" for="ativo">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" id="ativo"
+                           name="ativo" for="ativo">
                         Disciplina disponível
                     </label>
-                    <input type="checkbox" id="ativo" v-model="disciplinaAtual.ativo" class="form-checkbox h-5 w-5 text-orange-600">
+                    <input type="checkbox" id="ativo" v-model="disciplinaAtual.ativo"
+                           class="form-checkbox h-5 w-5 text-orange-600">
                 </div>
             </div>
             <div class="flex items-center justify-between">
-                <button class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                <button
+                    class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    type="submit">
                     Salvar
                 </button>
             </div>
