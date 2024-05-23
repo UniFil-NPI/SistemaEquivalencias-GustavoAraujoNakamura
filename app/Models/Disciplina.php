a34397ea1ec4fdc43e593fc3c056d3986087777c
Nakamura <gustavo.nakamura@edu.unifil.br>
2024-05-23 19:29:13 -0300

    fit adicionando modalidade no $fillable

--- app/Models/Disciplina.php
+++ app/Models/Disciplina.php
@@ -5,5 +5,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Disciplina extends Model
 {
-    protected $fillable = ['codigo', 'titulo' , 'tipo' , 'periodo' , 'carga_horaria', 'ativo' ];
+    protected $fillable = ['codigo', 'titulo' , 'tipo' , 'periodo' , 'carga_horaria', 'ativo', 'modalidade' ];
 }
