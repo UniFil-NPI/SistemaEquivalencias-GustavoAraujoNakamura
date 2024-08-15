6a27b6508bb9105db3b030997281514c743860bb
Nakamura <gustavo.nakamura@edu.unifil.br>
2024-08-15 13:25:09 -0300

    arrumando rota

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DisciplinaController;
+use App\Http\Controllers\GradeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -25,14 +26,18 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('/disciplina', DisciplinaController::class);
-Route::get('/disciplina/{id}', [DisciplinaController::class, 'show'])->name('disciplina.show');
-Route::patch('/disciplina/{id}', [DisciplinaController::class, 'update'])->name('disciplina.update');
-Route::delete('/disciplina/{disciplina}', [DisciplinaController::class, 'destroy'])->name('disciplina.destroy');
-Route::put('/disciplina/{id}', [DisciplinaController::class, 'update'])->name('disciplina.update');
-
-Route::post('/disciplina', [DisciplinaController::class, 'create'])->name('disciplina.create');
-Route::post('/disciplina', [DisciplinaController::class, 'store'])->name('disciplina.store');
-// preciso fazer route index
- Route::get('/disciplina', [DisciplinaController::class, 'index'])->name('disciplina.index');
 require __DIR__.'/auth.php';
+
+Route::resource('/disciplina', DisciplinaController::class);
+//Route::get('/disciplina/{id}', [DisciplinaController::class, 'show'])->name('disciplina.show');
+//Route::patch('/disciplina/{id}', [DisciplinaController::class, 'update'])->name('disciplina.update');
+//Route::delete('/disciplina/{disciplina}', [DisciplinaController::class, 'destroy'])->name('disciplina.destroy');
+//Route::put('/disciplina/{id}', [DisciplinaController::class, 'update'])->name('disciplina.update');
+//Route::post('/disciplina', [DisciplinaController::class, 'create'])->name('disciplina.create');
+//Route::post('/disciplina', [DisciplinaController::class, 'store'])->name('disciplina.store');
+//Route::get('/disciplina', [DisciplinaController::class, 'index'])->name('disciplina.index');
+Route::resource('/grade', GradeController::class);
+Route::get('/grades', [GradeController::class, 'index'])->name('grades.index');
+
+
+
