86b9d6db07ddd41af5bfee82b8af8459bd70f852
Nakamura <gustavo.nakamura@edu.unifil.br>
2024-08-22 17:45:55 -0300

    pedro2

--- app/Models/Disciplina.php
+++ app/Models/Disciplina.php
@@ -3,9 +3,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use App\Models\Grade;
 
 class Disciplina extends Model
 {
     use SoftDeletes;
     protected $fillable = ['codigo', 'titulo' , 'tipo' , 'periodo' , 'carga_horaria', 'ativo', 'modalidade' ];
+
+    public function grades(): BelongsToMany
+    {
+        return $this->belongsToMany(Grade::class);
+    }
 }
