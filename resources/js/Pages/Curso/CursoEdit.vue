766ed127889824b060067fefa9b43653046d2d17
Nakamura <gustavo.nakamura@edu.unifil.br>
2024-08-23 11:24:54 -0300

    curso edti started

---
+++ resources/js/Pages/Curso/CursoEdit.vue
@@ -0,0 +1,34 @@
+<template>
+    <CursoCreate :curso="curso" :isEditing="true"/>
+</template>
+
+<script>
+import CursoCreate from './CursoCreate.vue';
+import axios from 'axios';
+import { usePage } from '@inertiajs/vue3';
+
+export default {
+    components: {
+        CursoCreate,
+    },
+    data() {
+        return {
+            curso: { id: '', titulo: '', ano: '', ativo: false },
+        };
+    },
+    async mounted() {
+        await this.carregarCurso();
+    },
+    methods: {
+        async carregarCurso() {
+            const page = usePage();
+            try {
+                const response = await axios.get(route('curso.show', page.props.curso.id));
+                this.curso = response.data;
+            } catch (error) {
+                console.error("Erro ao carregar a curso:", error);
+            }
+        },
+    },
+};
+</script>
