a784a134840761ed188583ecb49732fe364d1c07
Nakamura <gustavo.nakamura@edu.unifil.br>
2024-08-27 20:04:23 -0300

    fit X routes

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,7 @@ Route::get('/grade/create', [GradeController::class, 'create'])->name('grade.cre
 Route::put('/grade/{id}', [GradeController::class, 'update'])->name('grade.update');
 
 Route::resource('/curso', CursoController::class);
+
 //Route::get('/cursos', [CursoController::class, 'index'])->name('cursos.index');
 //Route::get('/curso/create', [CursoController::class, 'create'])->name('curso.create');
 //Route::put('/curso/{id}', [CursoController::class, 'update'])->name('curso.update');
