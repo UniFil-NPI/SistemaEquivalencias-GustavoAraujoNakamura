ae6853a4d838062d8b6965d7aaa89c6c1f6f381d
Nakamura <gustavo.nakamura@edu.unifil.br>
2024-10-17 16:02:23 -0300

    equivalencia controller

---
+++ app/Http/Controllers/EquivalenciaController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Equivalencia;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Inertia\Inertia;
+
+class EquivalenciaController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('Equivalencia/Equivalencia', [
+            'equivalencia' => Equivalencia::all()
+        ]);
+    }
+
+    public function create()
+    {
+        return Inertia::render('Equivalencia/EquivalenciaCreate');
+    }
+
+    public function edit($id)
+    {
+        $equivalencia = Equivalencia::findOrFail($id);
+        return Inertia::render('Equivalencia/EquivalenciaEdit', [
+            'equivalencia' => $equivalencia
+        ]);
+    }
+
+    public function show($id)
+    {
+        $equivalencia = Equivalencia::find($id);
+        return response()->json($equivalencia);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'codigo' => 'required|unique:equivalencias',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('equivalencia/create')
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $equivalencia = Equivalencia::create($request->all());
+        $equivalencia = $equivalencia->save();
+
+        return redirect()->route('equivalencia.index');
+    }
+
+    public function update(Request $request, Equivalencia $equivalencia)
+    {
+        $equivalencia->update($request->all());
+        return redirect()->route('equivalencia.index');
+    }
+
+    public function destroy(Equivalencia $equivalencia)
+    {
+        $equivalencia->delete();
+        return response()->json(['message' => 'Equivalencia deletada com sucesso']);
+    }
+}
