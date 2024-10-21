c65bb32eaa133756254c333c260745bd855b52a0
Nakamura <gustavo.nakamura@edu.unifil.br>
2024-10-20 23:01:14 -0300

    DisciplinaEquivalencia Models

---
+++ app/Models/DisciplinaEquivalencia.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class DisciplinasEquivalencias extends Model
+{
+    use HasFactory;
+
+    protected $table = 'disciplinas_equivalencias';
+
+    protected $fillable = [
+        'disciplinas_id',
+        'equivalencias_id'
+    ];
+}
