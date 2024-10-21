e9186555eb99f95363a194eb54126167c57fa30e
Nakamura <gustavo.nakamura@edu.unifil.br>
2024-10-20 22:54:44 -0300

    resultado models

---
+++ app/Models/Resultado.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Resultados extends Model
+{
+    use HasFactory;
+    // use SoftDeletes;
+
+    protected $table = 'resultados';
+
+    protected $fillable = [
+        'titulo',
+        'grade_antiga',
+        'grade_nova'
+    ];
+
+    public function disciplinasCursadas()
+    {
+        return $this->belongsToMany(Disciplinas::class, 'resultados_disciplinas', 'resultados_id', 'disciplinas_id')
+            ->wherePivot('tipo', 'cursada')
+            ->withPivot('carga_horaria');
+
+    }
+
+    public function disciplinasAbatidas()
+    {
+        return $this->belongsToMany(Disciplinas::class, 'resultados_disciplinas', 'resultados_id', 'disciplinas_id')
+            ->wherePivot('tipo', 'abatida');
+    }
+
+    public function disciplinasAtribuidas()
+    {
+        return $this->belongsToMany(Disciplinas::class, 'resultados_disciplinas', 'resultados_id', 'disciplinas_id')
+            ->wherePivot('tipo', 'atribuida');
+    }
+
+    public function gradeAntiga()
+    {
+        return $this->belongsTo(Grades::class, 'grade_antiga');
+    }
+
+    public function gradeNova()
+    {
+        return $this->belongsTo(Grades::class, 'grade_nova');
+    }
+}
