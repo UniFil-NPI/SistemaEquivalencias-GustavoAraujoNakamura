63fdc2eab48de312f1a5e4f5dbc5abb3e5b760bc
Nakamura <gustavo.nakamura@edu.unifil.br>
2024-11-03 16:25:12 -0300

    fix aluno(a)

--- resources/js/Pages/GerarEquivalencia/GerarEquivalencia.vue
+++ resources/js/Pages/GerarEquivalencia/GerarEquivalencia.vue
@@ -49,7 +49,7 @@ const deleteGerarEquivalencia = async () => {
 
             <DataTable :value="gerarEquivalencias" paginator :rows="10">
                 <Column field="titulo" header="Título"></Column>
-                <Column field="user_id" header="Aluno"></Column>
+                <Column field="user_id" header="Aluno(a)"></Column>
                 <Column field="curso_antigo" header="Curso Antigo"></Column>
                 <Column field="curso" header="Curso Novo"></Column>
                 <Column field="grade_antiga" header="Grade Antiga"></Column>
